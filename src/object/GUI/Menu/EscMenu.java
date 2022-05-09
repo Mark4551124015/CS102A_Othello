30601f66ab14e3c93e0372d97867bd460da280e2
Mark4551124015 <mark4551124015@gmail.com>
2022-05-09 14:05:22 +0800

    fixed alot

---
+++ src/object/GUI/Menu/EscMenu.java
@@ -0,0 +1,116 @@
+package object.GUI.Menu;
+
+import component.animation.Animator;
+import graphics.Image;
+import object.GUI.Buttons.NormalButton;
+import object.OthelloObject;
+
+public class EscMenu extends OthelloObject {
+    private OthelloObject EscMenu;
+    private NormalButton Quit;
+    private NormalButton Save;
+    private NormalButton Load;
+    private NormalButton Restart;
+    private Animator EscMenuA;
+    private Animator EscMenuB;
+    private boolean wantBack;
+    private boolean wantSave;
+    private boolean wantReload;
+    private boolean wantRestart;
+
+    public EscMenu(){
+        super("GamePause");
+
+        this.EscMenu = new OthelloObject("EscMenu");
+        this.setImage(new Image("User_info_backGround"));
+        this.setSize(500,150);
+        this.EscMenu.setAlpha(0);
+
+        this.Quit = new NormalButton("Quit");
+        this.Quit.setActive(false);
+        this.Quit.setPosition(0,120);
+
+        this.Save = new NormalButton("Save");
+        this.Save.setActive(false);
+        this.Save.setPosition(0,40);
+
+        this.Load = new NormalButton("Load");
+        this.Load.setActive(false);
+        this.Load.setPosition(0,-40);
+
+        this.Restart = new NormalButton("Restart");
+        this.Restart.setActive(false);
+        this.Restart.setPosition(0,-120);
+
+        this.addObj(this.EscMenu);
+        this.addObj(this.Quit);
+        this.addObj(this.Save);
+        this.addObj(this.Load);
+        this.addObj(this.Restart);
+
+        this.EscMenuA = new Animator(0);
+        this.EscMenuB = new Animator(0);
+        this.addComponent(this.EscMenuA);
+        this.addComponent(this.EscMenuB);
+    }
+
+    public void setEscMenuAlpha(int a){
+        this.EscMenu.setAlpha(a);
+    }
+
+    public void setEscMenuActive(boolean flag){
+        this.Quit.setActive(flag);
+        this.Save.setActive(flag);
+        this.Load.setActive(flag);
+        this.Restart.setActive(flag);
+    }
+
+    public boolean isWantBack(){
+        return this.wantBack;
+    }
+
+    public boolean isWantSave(){
+        return this.wantSave;
+    }
+
+    public boolean isWantReload(){
+        return this.wantReload;
+    }
+
+    public boolean isWantRestart(){
+        return this.wantRestart;
+    }
+
+    public void setQuit(boolean flag){
+        this.wantBack = flag;
+    }
+
+    public void setEscSave(boolean flag){
+        this.wantSave = flag;
+    }
+
+    public void setLoad(boolean flag){
+        this.wantReload = flag;
+    }
+
+    public void setRestart(boolean flag){
+        this.wantRestart = flag;
+    }
+
+    @Override
+    public void update(double dt) {
+        super.update(dt);
+        if(this.Quit.isClicked()){
+            this.wantBack = true;
+        }
+        if(this.Save.isClicked()){
+            this.wantSave = true;
+        }
+        if(this.Load.isClicked()){
+            this.wantReload = true;
+        }
+        if(this.Restart.isClicked()){
+            this.wantRestart = true;
+        }
+    }
+}
---
+++ src/stage/transition/FadeTransition.java
@@ -0,0 +1,70 @@
+
+package stage.transition;
+
+import component.animation.Animation;
+import component.animation.Animator;
+import graphics.Shape;
+import main.mainApp;
+
+import java.awt.*;
+
+public class FadeTransition implements Transition {
+
+    private boolean active;
+    private Animator alphaAnimator;
+    private Shape pc;
+
+
+    public FadeTransition(Color color, double fadeTime, double delay, FadeType type) {
+        this.pc = new Shape("bg", color, Shape.Type.Rect, mainApp.WinSize);
+        if (type == FadeType.IN) {
+            this.pc.setAlpha(1);
+            this.alphaAnimator = new Animator(1);
+            this.alphaAnimator.append(Animation.GetTanh(1, 0, fadeTime, false, delay));
+        }
+
+        if (type == FadeType.OUT) {
+            this.pc.setAlpha(0);
+            this.alphaAnimator = new Animator(0);
+            this.alphaAnimator.append(Animation.GetTanh(0, 1, fadeTime, false, delay));
+        }
+
+
+        this.pc.setPosition(mainApp.Width / 2.0, mainApp.Height / 2.0);
+        this.alphaAnimator.setActive(false);
+        this.active = false;
+    }
+
+
+
+    @Override
+    public void init() {
+        this.active = true;
+        this.alphaAnimator.setActive(true);
+    }
+
+    @Override
+    public void update(double dt) {
+        if (!this.active)
+            return;
+        this.alphaAnimator.update(dt);
+        this.pc.setAlpha(this.alphaAnimator.val());
+    }
+
+    @Override
+    public void render(Graphics2D g2d) {
+        if (!this.active)
+            return;
+        this.pc.render(g2d);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return this.alphaAnimator.isIdle();
+    }
+
+    public enum FadeType {
+        IN,OUT
+    }
+
+}
